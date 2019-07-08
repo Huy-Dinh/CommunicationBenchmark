f3394317ff2cc301098439217eebdde38f443099
dinh.huyquoc <dinhquochuy.eeit@gmail.com>
2019-07-08 17:16:33 +0200

    Change to make sure the control words are sent

--- c_src/BenchmarkSender.c
+++ c_src/BenchmarkSender.c
@@ -41,14 +41,16 @@ void runThroughputTest(BenchmarkSender_t* pSender)
             if (pSender->mTestCases[pSender->mCurrentTestCase].mSendResult.noOfPacketsSent == 0)
             {
                 unsigned char controlWord[2] = {(unsigned char) BENCHMARK_CTRL_START_CASE, pSender->mCurrentTestCase};
-                (*pSender->pSendFunction)(controlWord, 2);
+                if ((*pSender->pSendFunction)(controlWord, 2) == BENCHMARK_SEND_FAIL)
+                    return;
                 pSender->mTestCases[pSender->mCurrentTestCase].timeTaken = pSender->pGetTickFunction();
             }
             BenchmarkSendResult_t sendResult = runThroughputTestCase(&(pSender->mTestCases[pSender->mCurrentTestCase]), pSender->pSendFunction);
             if (sendResult.verdict != BENCHMARK_SEND_UNDECIDED)
             {
                 unsigned char controlWord[2] = {(unsigned char) BENCHMARK_CTRL_END_CASE, pSender->mCurrentTestCase};
-                (*pSender->pSendFunction)(controlWord, 2);
+                if((*pSender->pSendFunction)(controlWord, 2) == BENCHMARK_SEND_FAIL)
+                    return;
 
                 pSender->mTestCases[pSender->mCurrentTestCase].timeTaken = pSender->pGetTickFunction() - pSender->mTestCases[pSender->mCurrentTestCase].timeTaken;
                 printSendResult(&(pSender->mTestCases[pSender->mCurrentTestCase]));
